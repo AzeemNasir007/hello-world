ae32b18b6f10b8666bdfa5b4a05d26245be91cd5
AzeemNasir007 <azeem_nasir@outlook.com>
2022-12-06 19:00:19 -0800

    Create Chat.js
    added chat.js

---
+++ components/Chat.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { View, Text, Button, TextInput, StyleSheet } from "react-native";
+
+export default class Chat extends React.Component {
+  componentDidMount() {
+    let name = this.props.route.params.name;
+    this.props.navigation.setOptions({ title: name });
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Button
+          title="Go to Start"
+          onPress={() => this.props.navigation.navigate("Start")}
+        />
+      </View>
+    );
+  };
+}
+
+
+
